060c5b922c5341c00c4a67ffe8a32fb535d30ca3
Jacek Czupyt <jacek.czupyt@besidethepark.com>
2024-06-12 16:17:24 +0200

    WIP Day 1 Task 2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::{env, fs};
+use onig::Regex;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    
+
     let file_path = args.get(2).map_or("input.txt".to_string(), |s| s.to_owned());
     let input = fs::read_to_string(file_path).unwrap();
-    
+
     let task = args.get(1).map_or(1,  |x| x.parse().unwrap());
-    
-    let result = match task { 
+
+    let result = match task {
         1 => task1(&input),
         2 => task2(&input),
         _ => panic!("invalid task")
     };
-    
+
     println!("{}", result)
 }
 
@@ -28,10 +29,14 @@ fn task1(input: &str) -> i32 {
         }).unwrap();
         String::from_iter(vec![c1, c2])
     }).collect();
-    
+
     results.iter().map(|x| x.parse::<i32>().unwrap()).sum()
 }
 
 fn task2(input: &str) -> i32 {
+    let mut lines = input.split('\n');
+    // let res: Vec<_>  = "abcabca".match_indices("abca").collect();
+    let res: Vec<_> = Regex::new("abca").unwrap().find_iter("abcabca").collect();
+    println!("{:?}", res);
     todo!()
 }
